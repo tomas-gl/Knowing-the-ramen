d5aab9bf9894c1d73f239d17da763bea88aeee55
tgavrel <tomas.gavrel@hotmail.fr>
2020-01-06 13:01:54 +0100

    Added the first row of content in the primary section

---
+++ assets/js/collaps.js
@@ -0,0 +1,13 @@
+var coll = document.getElementsByClassName("recette");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
